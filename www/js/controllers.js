6383062c78795088273b60b9541f8b23960860ca
remikarlsen <remikarlsen@gmail.com>
2015-06-17 13:03:12 +0200

    Litt opprydding og formatering av kode

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,108 +1,109 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($ionicPlatform, $scope, ruterService, utilService, $cordovaDevice, $cordovaGeolocation) {
-	$scope.pingresponse = "Test";
+        .controller('DashCtrl', function ($ionicPlatform, $scope, ruterService, utilService, $cordovaDevice, $cordovaGeolocation) {
+            $scope.pingresponse = "Test";
 
-    ruterService.getPing().success(function(data){
-    	$scope.pingresponse = data;
-	});
+            ruterService.getPing().success(function (data) {
+                $scope.pingresponse = data;
+            });
 
-    ruterService.getTravels('3010200', '3010011', 5).success(function(data){
-    	$scope.majorstuenToJernbanetorget = data;
-	});	
-		
-	$scope.MyDestinations = ruterService.getMyDestinations();
-	$scope.myTravels = ruterService.getMyTravels();
-	$scope.myTravels = _.groupBy($scope.myTravels, function(item){return item.to.desc});
-	
-	/*
-	$ionicPlatform.ready(function() {
-            // getting device infor from $cordovaDevice
-            var device = $cordovaDevice.getDevice();
-            $scope.manufacturer = device.manufacturer;
-            $scope.model = device.model;
-            $scope.platform = device.platform;
-            $scope.uuid = device.uuid;      
-    });
-    */
-	
-	var myStops = ruterService.getMyStops();
-	
-	//DEBUG
-	var myStops = [{ID:'3010200', test:'a'}, {ID:'3010011', test:'b'}, {ID:'3011910', test:'c'}];
-	
-	ruterService.getStopInfo(myStops).then(function(data){
-		_.each(myStops, function(stop){
-			_.extend(stop, _.find(data, function(dataStop){return dataStop.ID}));
-		});
-    	$scope.myStops = myStops;
-  	})	
-	
-	var start = {
-		latitude: 38.898556,
-		longitude: -77.037852
-	};
+            ruterService.getTravels('3010200', '3010011', '').success(function (data) {
+                $scope.majorstuenToJernbanetorget = data;
+            });
 
-	var end = {
-		latitude: 38.897147,
-		longitude: -77.043934
-	};	
+            $scope.MyDestinations = ruterService.getMyDestinations();
+            $scope.myTravels = ruterService.getMyTravels();
+            $scope.myTravels = _.groupBy($scope.myTravels, function (item) {
+                return item.to.desc
+            });
 
-	$scope.distance = utilService.getDistance(start, end);
+            /*
+             $ionicPlatform.ready(function() {
+             // getting device infor from $cordovaDevice
+             var device = $cordovaDevice.getDevice();
+             $scope.manufacturer = device.manufacturer;
+             $scope.model = device.model;
+             $scope.platform = device.platform;
+             $scope.uuid = device.uuid;      
+             });
+             */
 
-	var posOptions = {timeout: 10000, enableHighAccuracy: false};
-	$scope.pos = {};
-	$cordovaGeolocation
-	.getCurrentPosition(posOptions)
-	.then(function (position) {
-	  $scope.pos.latitude  = position.coords.latitude
-	  $scope.pos.longitude = position.coords.longitude
-	}, function(err) {
-	  // error
-	});
+            var myStops = ruterService.getMyStops();
 
-	var watchOptions = {
-	frequency : 1000,
-	timeout : 3000,
-	enableHighAccuracy: false // may cause errors if true
-	};
+            //DEBUG
+            var myStops = [{ID: 3010200, test: 'a'}, {ID: 3010011, test: 'b'}, {ID: 3011910, test: 'c'}];
+            var tmp = [];
+            ruterService.getStopInfo(myStops).then(function (data) {
+                $scope.myStops = _.map(myStops, function (stop) {
+                    return _.extend(stop, _.findWhere(data, {ID: stop.ID}));
+                });
+            })
 
-	var watch = $cordovaGeolocation.watchPosition(watchOptions);
-	watch.then(
-	null,
-	function(err) {
-	  // error
-	},
-	function(position) {
-	  $scope.pos.latitude  = position.coords.latitude
-	  $scope.pos.longitude = position.coords.longitude
-	});
+            var start = {
+                latitude: 38.898556,
+                longitude: -77.037852
+            };
 
-	watch.clearWatch();
-	
-})
+            var end = {
+                latitude: 38.897147,
+                longitude: -77.043934
+            };
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+            $scope.distance = utilService.getDistance(start, end);
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+            var posOptions = {timeout: 10000, enableHighAccuracy: false};
+            $scope.pos = {};
+            $cordovaGeolocation
+                    .getCurrentPosition(posOptions)
+                    .then(function (position) {
+                        $scope.pos.latitude = position.coords.latitude
+                        $scope.pos.longitude = position.coords.longitude
+                    }, function (err) {
+                        // error
+                    });
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+            var watchOptions = {
+                frequency: 1000,
+                timeout: 3000,
+                enableHighAccuracy: false // may cause errors if true
+            };
+
+            var watch = $cordovaGeolocation.watchPosition(watchOptions);
+            watch.then(
+                    null,
+                    function (err) {
+                        // error
+                    },
+                    function (position) {
+                        $scope.pos.latitude = position.coords.latitude
+                        $scope.pos.longitude = position.coords.longitude
+                    });
+
+            watch.clearWatch();
+
+        })
+
+        .controller('ChatsCtrl', function ($scope, Chats) {
+            // With the new view caching in Ionic, Controllers are only called
+            // when they are recreated or on app start, instead of every page change.
+            // To listen for when this page is active (for example, to refresh data),
+            // listen for the $ionicView.enter event:
+            //
+            //$scope.$on('$ionicView.enter', function(e) {
+            //});
+
+            $scope.chats = Chats.all();
+            $scope.remove = function (chat) {
+                Chats.remove(chat);
+            }
+        })
+
+        .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+            $scope.chat = Chats.get($stateParams.chatId);
+        })
+
+        .controller('AccountCtrl', function ($scope) {
+            $scope.settings = {
+                enableFriends: true
+            };
+        });
--- www/js/services.js
+++ www/js/services.js
@@ -1,158 +1,137 @@
 angular.module('starter.services', [])
 
-.factory('ruterService', function($http, $q, $filter) {
+        .factory('ruterService', function ($http, $q, $filter) {
 
-	var MyDestinations = {
-		HJEM: 'Hjem',
-		JOBB: 'Jobb',
-		SKOLE: 'Skole',
-		BARNEHAGE: 'Barnehage'
-	};
+            var MyDestinations = {
+                HJEM: 'Hjem',
+                JOBB: 'Jobb',
+                SKOLE: 'Skole'
+            };
 
-	var myStops = {};
-	myStops['Majorstuen'] = {id:'3010200', desc:MyDestinations.HJEM};
-	myStops['Jernbanetorget'] = {id:'3010011', desc:MyDestinations.JOBB};
-	myStops['Rodtvedt'] = {id:'3011910', desc:MyDestinations.BARNEHAGE};
-	myStops['Veitvedt'] = {id:'3012040', desc:MyDestinations.SKOLE};
+            var myStops = {};
+            myStops['Majorstuen'] = {id: '3010200', desc: MyDestinations.HJEM};
+            myStops['Jernbanetorget'] = {id: '3010011', desc: MyDestinations.JOBB};
+            myStops['Veitvedt'] = {id: '3012040', desc: MyDestinations.SKOLE};
 
-	var myTravels = [
-	{name:'majorstuenToJernbanetorget', from:myStops['Majorstuen'], to:myStops['Jernbanetorget']},
-	{name:'rodtvedtToJernbanetorget', from:myStops['Rodtvedt'], to:myStops['Jernbanetorget']},
-	{name:'veitvedtToJernbanetorget', from:myStops['Veitvedt'], to:myStops['Jernbanetorget']},
-	{name:'jernbanetorgetToVeitvedt', from:myStops['Jernbanetorget'], to:myStops['Veitvedt']},
-	{name:'majorstuenToVeitvedt', from:myStops['Majorstuen'], to:myStops['Veitvedt']},	
-	{name:'majorstuenToRodtvedt', from:myStops['Majorstuen'], to:myStops['Rodtvedt']},
-	{name:'jernbanetorgetToRodtvedt', from:myStops['Jernbanetorget'], to:myStops['Rodtvedt']},
-	{name:'jernbanetorgetToMajorstuen', from:myStops['Jernbanetorget'], to:myStops['Majorstuen']},
-	{name:'veitvedtToMajorstuen', from:myStops['Veitvedt'], to:myStops['Majorstuen']},
-	{name:'rodvedtToMajorstuen', from:myStops['Rodtvedt'], to:myStops['Majorstuen']}	
-	]; 	
+            var myTravels = [
+                {name: 'majorstuenToJernbanetorget', from: myStops['Majorstuen'], to: myStops['Jernbanetorget']},
+                {name: 'veitvedtToJernbanetorget', from: myStops['Veitvedt'], to: myStops['Jernbanetorget']},
+                {name: 'jernbanetorgetToVeitvedt', from: myStops['Jernbanetorget'], to: myStops['Veitvedt']},
+                {name: 'majorstuenToVeitvedt', from: myStops['Majorstuen'], to: myStops['Veitvedt']},
+                {name: 'jernbanetorgetToMajorstuen', from: myStops['Jernbanetorget'], to: myStops['Majorstuen']},
+                {name: 'veitvedtToMajorstuen', from: myStops['Veitvedt'], to: myStops['Majorstuen']}
+            ];
 
-   return {
-      getStopInfo: function(stops){
-      var promises = stops.map(function(stop){     
-		  var deffered  = $q.defer();  
-		  $http({
-			url : 'http://reisapi.ruter.no/Place/GetStop/'+stop.ID+'?json=true',
-			method: 'GET'
-		  }).
-		  success(function(data){
-			deffered.resolve(data);
-		  }).
-		  error(function(error){
-			  deffered.reject();
-		  });
-		  return deffered.promise;
-    });
-    return $q.all(promises);
-  },
-
-	getMyStops: function() {
-     	return myStops;
-     },     
-   
-     getPing: function() {
-           return $http({
-            url: 'http://reisapi.ruter.no/heartbeat/index?json=true',
-            method: 'GET'
+            return {
+                getStopInfo: function (stops) {
+                    var promises = stops.map(function (stop) {
+                        var deffered = $q.defer();
+                        $http({
+                            url: 'http://reisapi.ruter.no/Place/GetStop/' + stop.ID + '?json=true',
+                            method: 'GET'
+                        }).
+                                success(function (data) {
+                                    deffered.resolve(data);
+                                }).
+                                error(function (error) {
+                                    deffered.reject();
+                                });
+                        return deffered.promise;
+                    });
+                    return $q.all(promises);
+                },
+                getMyStops: function () {
+                    return myStops;
+           },
+                getPing: function () {
+                    return $http({
+                        url: 'http://reisapi.ruter.no/heartbeat/index?json=true',
+                        method: 'GET'
+                    })
+                },
+                getMyTravels: function () {
+                    return myTravels;
+                },
+                getMyDestinations: function () {
+                    return MyDestinations;
+                },
+                getTravels: function (from, to, line) {
+                    var dateFilter = $filter('date');
+                    var filteredDate = dateFilter(new Date(), 'ddMMyyyyhhmmss')
+                    return $http({
+                        url: 'http://reisapi.ruter.no/Travel/GetTravels?fromplace=' + from + '&toplace=' + to + '&isafter=True&time=' + filteredDate + '&proposals=3&transporttypes=8&linenames=' + line,
+                        method: 'GET'
+                    })
+                },
+            }
         })
-     },
-    
-     getMyTravels: function() {
-     	return myTravels;
-     },
-     
-     getMyDestinations: function() {
-     	return MyDestinations;
-     },
-     
-     getTravels: function(from, to, line) {
-     	var dateFilter = $filter('date');
-		var filteredDate = dateFilter(new Date(), 'ddMMyyyyhhmmss')
-           return $http({
-            url: 'http://reisapi.ruter.no/Travel/GetTravels?fromplace='+from+'&toplace='+to+'&isafter=True&time='+filteredDate+'&proposals=1&transporttypes=8&linenames='+line,
-            method: 'GET'
+
+        .factory('utilService', function () {
+            var toRad = function (num) {
+                return num * Math.PI / 180;
+            };
+
+            return{//Haversine-algoritme
+                getDistance: function (start, end) {
+                    var EARTH_RADIUS_KM = 6371;
+                    var dLat = toRad(end.latitude - start.latitude);
+                    var dLon = toRad(end.longitude - start.longitude);
+                    var lat1 = toRad(start.latitude);
+                    var lat2 = toRad(end.latitude);
+                    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+                    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                    return EARTH_RADIUS_KM * c * 1000;
+                }
+            }
         })
-     },
-     
-     getTravels: function(from, to, line) {
-     	var dateFilter = $filter('date');
-		var filteredDate = dateFilter(new Date(), 'ddMMyyyyhhmmss')
-           return $http({
-            url: 'http://reisapi.ruter.no/Travel/GetTravels?fromplace='+from+'&toplace='+to+'&isafter=True&time='+filteredDate+'&proposals=1&transporttypes=8&linenames='+line,
-            method: 'GET'
-        })
-     },    
-     
-   }//return
-})
 
-.factory('utilService', function() {
-	var toRad = function (num) {
-		return num * Math.PI / 180;
-	};
+        .factory('Chats', function () {
+            // Might use a resource here that returns a JSON array
 
-	return{ //Haversine-algoritme
-		getDistance: function(start, end) {
-			var EARTH_RADIUS_KM = 6371;			
-			var dLat = toRad(end.latitude - start.latitude);
-			var dLon = toRad(end.longitude - start.longitude);
-			var lat1 = toRad(start.latitude);
-			var lat2 = toRad(end.latitude);
-			var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-				Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-			return EARTH_RADIUS_KM * c * 1000;
-		}
-   }
-})
+            // Some fake testing data
+            var chats = [{
+                    id: 0,
+                    name: 'Ben Sparrow',
+                    lastText: 'You on your way?',
+                    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+                }, {
+                    id: 1,
+                    name: 'Max Lynx',
+                    lastText: 'Hey, it\'s me',
+                    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+                }, {
+                    id: 2,
+                    name: 'Adam Bradleyson',
+                    lastText: 'I should buy a boat',
+                    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+                }, {
+                    id: 3,
+                    name: 'Perry Governor',
+                    lastText: 'Look at my mukluks!',
+                    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+                }, {
+                    id: 4,
+                    name: 'Mike Harrington',
+                    lastText: 'This is wicked good ice cream.',
+                    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+                }];
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  },{
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
-});
+            return {
+                all: function () {
+                    return chats;
+                },
+                remove: function (chat) {
+                    chats.splice(chats.indexOf(chat), 1);
+                },
+                get: function (chatId) {
+                    for (var i = 0; i < chats.length; i++) {
+                        if (chats[i].id === parseInt(chatId)) {
+                            return chats[i];
+                        }
+                    }
+                    return null;
+                }
+            };
+        });
 	
