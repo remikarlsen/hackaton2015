021d7c1791bfe732122b1451c48938bb24d90e7c
Magnus Raaum <magnus.raaum@sparebank1.no>
2015-06-17 16:12:27 +0200

    La til fargeendring på tiden

--- www/js/ctrl/overviewController.js
+++ www/js/ctrl/overviewController.js
@@ -24,10 +24,18 @@ angular.module('starter.controllers')
         _.each($scope.destinations, function (dest) {
             ruterService.getTravels($scope.home.id, dest.ID).success(function (data){
                 dest.proposals = _.map(data.TravelProposals, function (item) {
+                    var differ = new Number(Math.ceil((new Date($filter('date')(item.ArrivalTime, 'medium')).getTime() - new Date().getTime()) / 1000));
+                    var colorStep = 255 / differ;
+                    console.log(colorStep);
                     return {
                         time : item.ArrivalTime,
                         line : item.Stages[0].LineID,
-                        diff : new Number(Math.ceil((new Date($filter('date')(item.ArrivalTime, 'medium')).getTime() - new Date().getTime()) / 1000))
+                        diff : differ,
+                        color : {
+                            r: 0, g: 255, b: 0
+                        },
+                        colorHTML : 'rgb(0, 255, 0)',
+                        colorStep : colorStep
                     }
                 });
             });
@@ -36,12 +44,20 @@ angular.module('starter.controllers')
         $scope.onCountDown = function () {
             $scope.destinations = _.each($scope.destinations, function(dest) {
                 _.each(dest.proposals, function (props) {
-                    props.diff = props.diff - 1;
+                    if (props.diff !== 0) {
+                        props.diff = props.diff - 1;
+                    }
+                    props.colorHTML = $scope.getColor(props);
                 });
             });
             countdown = $timeout($scope.onCountDown, 1000);
         };
 
+        $scope.getColor = function (props) {
+          props.color.r = (props.color.r !== 255 || props.color.r > 255) ? (props.color.r + props.colorStep) : props.color.r;
+          props.color.g = (props.color.g !== 0 || props.color.g < 0) ? (props.color.g - props.colorStep) : props.color.g;
+           return 'rgb('+Math.ceil(props.color.r) +','+Math.ceil(props.color.g)+','+Math.ceil(props.color.b)+')';
+        };
         var countdown = $timeout($scope.onCountDown, 1000);
 
     });
