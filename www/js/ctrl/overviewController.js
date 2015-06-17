24324d0b726e15e0ca908bb2a9596a7ca1e1dc3e
Magnus Raaum <magnus.raaum@sparebank1.no>
2015-06-17 14:36:31 +0200

    Fikset småting

--- www/js/ctrl/overviewController.js
+++ www/js/ctrl/overviewController.js
@@ -8,22 +8,22 @@ angular.module('starter.controllers')
         $scope.destinations = [{
             name : 'Jernbanetorget'
         }, {
-            name : 'Rodtvedt'
+            name : 'Veitvedt'
         }];
 
-        var myStops = ruterService.getMyStops();
-
+        var myStops = ruterService.getMyStopsMap();
         $scope.home.desc = myStops[$scope.home.name].desc;
-        $scope.home.id = myStops[$scope.home.name].id;
+        $scope.home.id = myStops[$scope.home.name].ID;
 
         $scope.destinations = _.map($scope.destinations, function (dest) {
                 dest.desc = myStops[dest.name].desc;
-                dest.id = myStops[dest.name].id;
+                dest.ID = myStops[dest.name].ID;
             return dest;
         });
 
         _.each($scope.destinations, function (dest) {
-            ruterService.getTravels($scope.home.id, dest.id).success(function (data){
+            console.log(dest);
+            ruterService.getTravels($scope.home.id, dest.ID).success(function (data){
                 dest.proposals = _.map(data.TravelProposals, function (item) {
                     return {
                         time : item.ArrivalTime,
